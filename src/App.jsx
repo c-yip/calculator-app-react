3b187527e66c134852227686fd492057dd20e9c8
Christian <christianyip92@gmail.com>
2022-11-10 15:21:36 -0800

    Add value property to all button elements.
    Add input state and set display element's value to input state.

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,111 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState({
+    inputOne: "",
+    inputTwo: "",
+    operator: "",
+  });
+
+  function handleNumberClick(event) {
+    const value = event.target.value;
+    console.log(value);
+  }
+
+  function handleOperatorClick(event) {
+    const value = event.target.value;
+    console.log(value);
+  }
+
+  function handleAdditionalClick(event) {
+    const value = event.target.value;
+    console.log(value);
+  }
+
   return (
     <div className="App">
       <h1>Calculator App</h1>
       <div className="calculator">
         <div className="display">
-          <h2 className="display-input">
-            1234522222222222222222222222222222222
-          </h2>
+          <h2 className="display-input">{`${input.inputOne} ${input.operator} ${input.inputTwo}`}</h2>
         </div>
         <div className="buttons-container">
           <div className="num-buttons">
-            <button className="btn">7</button>
-            <button className="btn">8</button>
-            <button className="btn">9</button>
-            <button className="btn">4</button>
-            <button className="btn">5</button>
-            <button className="btn">6</button>
-            <button className="btn">1</button>
-            <button className="btn">2</button>
-            <button className="btn">3</button>
-            <button className="btn">0</button>
-            <button className="btn">.</button>
-            <button className="btn">=</button>
+            <button className="btn" value={7}>
+              7
+            </button>
+            <button className="btn" value={8}>
+              8
+            </button>
+            <button className="btn" value={9}>
+              9
+            </button>
+            <button className="btn" value={4}>
+              4
+            </button>
+            <button className="btn" value={5}>
+              5
+            </button>
+            <button className="btn" value={6}>
+              6
+            </button>
+            <button className="btn" value={1}>
+              1
+            </button>
+            <button className="btn" value={2}>
+              2
+            </button>
+            <button className="btn" value={3}>
+              3
+            </button>
+            <button className="btn" value={0}>
+              0
+            </button>
+            <button className="btn" value={"."}>
+              .
+            </button>
+            <button className="btn" value={"="}>
+              =
+            </button>
           </div>
 
           <div className="op-buttons">
-            <button className="btn">/</button>
-            <button className="btn">X</button>
-            <button className="btn">-</button>
-            <button className="btn">+</button>
+            <button className="btn" value={"/"}>
+              /
+            </button>
+            <button className="btn" value={"x"}>
+              X
+            </button>
+            <button className="btn" value={"-"}>
+              -
+            </button>
+            <button className="btn" value={"+"}>
+              +
+            </button>
           </div>
 
           <div className="additional-buttons">
-            <button className="btn">AC</button>
-            <button className="btn">+/-</button>
-            <button className="btn">%</button>
-            <button className="btn">^</button>
-            <button className="btn">(</button>
-            <button className="btn">)</button>
-            <button className="btn">DEL</button>
+            <button className="btn" value={"ac"}>
+              AC
+            </button>
+            <button className="btn" value={"+/-"}>
+              +/-
+            </button>
+            <button className="btn" value={"%"}>
+              %
+            </button>
+            <button className="btn" value={"^"}>
+              ^
+            </button>
+            <button className="btn" value={"("}>
+              (
+            </button>
+            <button className="btn" value={")"}>
+              )
+            </button>
+            <button className="btn" value={"del"}>
+              DEL
+            </button>
           </div>
         </div>
       </div>
